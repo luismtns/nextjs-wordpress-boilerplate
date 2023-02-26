2826323f48520899ec9547d2778acb3e7a62b7c8
Luís Otávio Bovo <luis@fillet.com.br>
2023-02-26 14:41:14 -0300

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { ApolloProvider } from "@apollo/client/react";
+import { client } from "../lib/apollo";
+import "../styles/index.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />;
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
